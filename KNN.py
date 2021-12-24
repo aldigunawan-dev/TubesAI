c118188c334bcfbf5df3e51d1c9f3a4acd86dbc2
aldigunawan <78894332+aldigunawan-dev@users.noreply.github.com>
2021-12-24 14:28:40 +0700

    fix code, rapihin dikit. Hapus test file

--- KNN.py
+++ KNN.py
@@ -1,37 +1,25 @@
-# k-nearest neighbors on the Iris Flowers Dataset
 from random import seed
 from random import randrange
 from csv import reader
 from math import sqrt
- 
-# Load a CSV file
-def load_csv(filename):
+
+# Load CSV file
+def load_csv(file):
 	dataset = list()
-	with open(filename, 'r') as file:
+	with open(file, 'r') as file:
 		csv_reader = reader(file)
 		for row in csv_reader:
 			if not row:
 				continue
 			dataset.append(row)
 	return dataset
- 
+
 # Convert string column to float
-def str_column_to_float(dataset, column):
+def string_ke_float(dataset, column):
 	for row in dataset:
 		row[column] = float(row[column].strip())
- 
-# Convert string column to integer
-def str_column_to_int(dataset, column):
-	class_values = [row[column] for row in dataset]
-	unique = set(class_values)
-	lookup = dict()
-	for i, value in enumerate(unique):
-		lookup[value] = i
-	for row in dataset:
-		row[column] = lookup[row[column]]
-	return lookup
- 
-# Find the min and max values for each column
+
+# minmax
 def dataset_minmax(dataset):
 	minmax = list()
 	for i in range(len(dataset[0])):
@@ -40,15 +28,15 @@ def dataset_minmax(dataset):
 		value_max = max(col_values)
 		minmax.append([value_min, value_max])
 	return minmax
- 
-# Rescale dataset columns to the range 0-1
-def normalize_dataset(dataset, minmax):
+
+# Normalisasi data
+def normalisasi_dataset(dataset, minmax):
 	for row in dataset:
 		for i in range(len(row)):
 			row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
- 
-# Split a dataset into k folds
-def cross_validation_split(dataset, n_folds):
+
+# Bagi dataset menjadi n bagian
+def bagi_data(dataset, n_folds):
 	dataset_split = list()
 	dataset_copy = list(dataset)
 	fold_size = int(len(dataset) / n_folds)
@@ -59,19 +47,19 @@ def cross_validation_split(dataset, n_folds):
 			fold.append(dataset_copy.pop(index))
 		dataset_split.append(fold)
 	return dataset_split
- 
-# Calculate accuracy percentage
-def accuracy_metric(actual, predicted):
+
+# Hitung akurasi
+def hitung_akurasi(actual, predicted):
 	correct = 0
 	for i in range(len(actual)):
 		if actual[i] == predicted[i]:
 			correct += 1
 	return correct / float(len(actual)) * 100.0
- 
-# Evaluate an algorithm using a cross validation split
-def evaluate_algorithm(dataset, algorithm, n_folds, *args):
-	folds = cross_validation_split(dataset, n_folds)
-	scores = list()
+
+# Evaluasi dengan cross validation split
+def evaluasi(dataset, algorithm, n_folds, *args):
+	folds = bagi_data(dataset, n_folds)
+	skor = list()
 	for fold in folds:
 		train_set = list(folds)
 		train_set.remove(fold)
@@ -83,55 +71,52 @@ def evaluate_algorithm(dataset, algorithm, n_folds, *args):
 			row_copy[-1] = None
 		predicted = algorithm(train_set, test_set, *args)
 		actual = [row[-1] for row in fold]
-		accuracy = accuracy_metric(actual, predicted)
-		scores.append(accuracy)
-	return scores
- 
-# Calculate the Euclidean distance between two vectors
-def euclidean_distance(row1, row2):
+		accuracy = hitung_akurasi(actual, predicted)
+		skor.append(accuracy)
+	return skor
+
+# Jarak Euclidean
+def jarak_euclidian(row1, row2):
 	distance = 0.0
 	for i in range(len(row1)-1):
 		distance += (row1[i] - row2[i])**2
 	return sqrt(distance)
- 
-# Locate the most similar neighbors
-def get_neighbors(train, test_row, num_neighbors):
+
+# Mencari tetangga
+def cari_tetangga(train, test_row, num_neighbors):
 	distances = list()
 	for train_row in train:
-		dist = euclidean_distance(test_row, train_row)
+		dist = jarak_euclidian(test_row, train_row)
 		distances.append((train_row, dist))
 	distances.sort(key=lambda tup: tup[1])
 	neighbors = list()
 	for i in range(num_neighbors):
 		neighbors.append(distances[i][0])
 	return neighbors
- 
-# Make a prediction with neighbors
-def predict_classification(train, test_row, num_neighbors):
-	neighbors = get_neighbors(train, test_row, num_neighbors)
+
+# Buat prediksi
+def prediksi_kelas(train, test_row, num_neighbors):
+	neighbors = cari_tetangga(train, test_row, num_neighbors)
 	output_values = [row[-1] for row in neighbors]
 	prediction = max(set(output_values), key=output_values.count)
 	return prediction
- 
-# kNN Algorithm
-def k_nearest_neighbors(train, test, num_neighbors):
+
+# Algoritma kNN
+def KNN(train, test, num_neighbors):
 	predictions = list()
 	for row in test:
-		output = predict_classification(train, row, num_neighbors)
+		output = prediksi_kelas(train, row, num_neighbors)
 		predictions.append(output)
 	return(predictions)
- 
-# Test the kNN on the Iris Flowers dataset
+
+# Test kNN di dataset diabetes
 seed(1)
-filename = 'diabetes.csv'
-dataset = load_csv(filename)
+file = 'diabetes.csv'
+dataset = load_csv(file)
 for i in range(len(dataset[0])-1):
-	str_column_to_float(dataset, i)
-# convert class column to integers
-str_column_to_int(dataset, len(dataset[0])-1)
-# evaluate algorithm
+	string_ke_float(dataset, i)
 n_folds = 5
 num_neighbors = 21
-scores = evaluate_algorithm(dataset, k_nearest_neighbors, n_folds, num_neighbors)
-print('Scores: %s' % scores)
-print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+skor = evaluasi(dataset, KNN, n_folds, num_neighbors)
+print('Akurasi: %s' % skor)
+print('Rata-rata Akurasi: %.3f%%' % (sum(skor)/float(len(skor))))
